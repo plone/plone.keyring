863db82334449a3c5584cc2e79c11302f2cf7241
Hanno Schlichting <hanno@hannosch.eu>
2010-07-16 14:00:30 +0000

    Update package information.
    svn path=/plone.keyring/trunk/; revision=37896

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '2.0b2'
+version = '2.0'
 
 setup(name='plone.keyring',
       version=version,
@@ -8,12 +8,15 @@ setup(name='plone.keyring',
       long_description=open("README.txt").read() + "\n" + \
                        open("CHANGES.txt").read(),
       classifiers=[
-        "Framework :: Plone",
-        "Framework :: Zope2",
-        "Programming Language :: Python",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
         ],
       keywords='',
-      author='Wichert Akkerman',
+      author='Plone Foundation',
       author_email='plone-developers@lists.sourceforge.net',
       url='http://pypi.python.org/pypi/plone.keyring',
       license='BSD',
