250b0e462af52707cf50803a86a9720b6a85720a
Maurits van Rees <maurits@vanrees.org>
2018-09-27 21:22:50 +0200

    Added keywords in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         ],
-    keywords='',
+    keywords='keyring security secrets',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
     url='https://pypi.python.org/pypi/plone.keyring',
