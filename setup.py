578e6092ad6451b6e489447761e3da5a29f0cbd9
Maurits van Rees <maurits@vanrees.org>
2018-09-27 21:22:19 +0200

    Removed Plone 5.0 and 5.1 from classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
-        "Framework :: Plone :: 5.0",
-        "Framework :: Plone :: 5.1",
         "Framework :: Zope2",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
