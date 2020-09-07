83fa5dfe594ee06b64638c23dd466a457a25392d
Maurits van Rees <maurits@vanrees.org>
2020-09-07 13:23:34 +0200

    Removed Plone 4.3 and 5.0 from classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone :: 4.3",
-        "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
         "Framework :: Plone :: 5.2",
         "Framework :: Plone :: Core",
