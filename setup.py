2ef3d23a0c7358f31dbfad181f9995b8082871a8
Maurits van Rees <maurits@vanrees.org>
2023-02-08 00:15:08 +0100

    Add python_requires metadata.
    [ci skip]

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
@@ -37,6 +36,7 @@ setup(
     namespace_packages=['plone'],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
     install_requires=[
         'setuptools',
         'ZODB3',
