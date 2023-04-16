e81a46ea7ae84ded6e63b86f58b1f25410b1f069
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-04-16 13:06:28 +0200

    feat: only python3.8+

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     namespace_packages=["plone"],
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
+    python_requires=">=3.8",
     install_requires=[
         "setuptools",
         "ZODB",
